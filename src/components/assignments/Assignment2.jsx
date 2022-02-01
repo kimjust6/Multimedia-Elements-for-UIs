639c0aaacd4b691c9bb42efa235674dcdfa4d914
Justin Kim <mletemps@hotmail.com>
2022-02-01 03:06:54 -0500

    added random images

--- src/components/assignments/Assignment2.jsx
+++ src/components/assignments/Assignment2.jsx
@@ -8,7 +8,11 @@ const Assignment2 = () => {
     <div className="container">
 
     <h1>Assignment 2</h1>
-    <p>Assignment 2 TODO.</p>
+    <div className="images">
+      <img src="https://i.imgur.com/0m42NUm.jpg" alt="Firecracker"></img>
+      <img src="https://i.imgur.com/gyvSmgb.jpg" alt="Pandemic"></img>
+      <img src="https://i.imgur.com/0m42NUm.jpg" alt="Firecracker"></img>
+    </div>
     
     
   </div>
