c0dd1452e6b1f7b3a3398123965f6f176d202fce
unknown <mletemps@hotmail.com>
2022-04-10 11:43:59 -0400

    completed assignment 2 and added scss styling file

--- src/components/assignments/Assignment2.jsx
+++ src/components/assignments/Assignment2.jsx
@@ -1,19 +1,34 @@
 
 import React from 'react';
+import "./assignment2.scss";
 import AssignmentsNav from './AssignmentsNav';
 
 
 const Assignment2 = () => {
   return (
     <div className="container">
+      <h1>Assignment 2</h1>
 
-    <h1>Assignment 2</h1>
-    <p className="text-sm">Assignment 1 TODO.</p>
-    
-    
-    
-  </div>
-    );
+      <div className="assignment2">
+        <div className="text">
+          <h2>Music</h2>
+          <p>
+          YouTube. (2018). Intro : Serendipity 여자 Cover by Line.B. YouTube. Retrieved April 10, 2022, from <a href="https://www.youtube.com/watch?v=fTSgLiGLYJI" target="_blank">https://www.youtube.com/watch?v=fTSgLiGLYJI. </a>
+          </p>
+          <h2>Equipment</h2>
+          <p>
+            <li>Smartphone</li>
+            <li> Laptop </li>
+          </p>
+        </div>
+        <div className="content">
+          <div className="video">
+            <iframe width="1904" height="772" src="https://www.youtube.com/embed/hrtTphVENC0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Assignment2;
