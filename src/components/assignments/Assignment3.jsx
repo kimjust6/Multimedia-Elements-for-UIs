2f5df1203d77dd3102dfae49a617d386e71ec737
unknown <mletemps@hotmail.com>
2022-04-18 09:54:28 -0400

    fixed file names

--- src/components/assignments/Assignment3.jsx
+++ src/components/assignments/Assignment3.jsx
@@ -1,17 +1,24 @@
-
-import React from 'react';
-import A3P5 from './Assignment3P5';
-
+import React from "react";
+import A3P5 from "./Assignment3P5";
 
 const Assignment3 = () => {
   return (
     <div className="container">
+      <button class="btn btn-primary" onClick={getRandomInt}>Button</button>
+      <h1>Assignment 3</h1>
+      <A3P5 />
+      <br />
 
-    <h1>Assignment 3</h1>
-    <A3P5/>
-    
-  </div>
-    );
+      <br />
+    </div>
+  );
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 };
+
+
 
 export default Assignment3;
