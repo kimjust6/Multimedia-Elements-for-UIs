75a3e880ead09ad12535b158a6662db7266dfdd7
unknown <mletemps@hotmail.com>
2022-04-19 16:01:43 -0400

    finished snake game and added a reflection

--- src/components/assignments/Assignment3.jsx
+++ src/components/assignments/Assignment3.jsx
@@ -1,6 +1,11 @@
+import { Button } from 'react-bootstrap'
+
 import React from "react";
 import A3P5 from "./Assignment3P5";
 
+const reloadPage = () => {
+  window.location.reload()
+}
 const Assignment3 = () => {
   return (
     <div className="container">
@@ -8,15 +13,18 @@ const Assignment3 = () => {
       <h1>Assignment 3</h1>
       <A3P5 />
       <br />
-
+      <Button className="primary" onClick={reloadPage}>Try Again</Button>
       <br />
+      <br />
+      <h1>Reflection</h1>
+      <p>I created a snake game that you can control using arrows. The goal is to eat as many timbits as possible to improve your score.</p>
+      <br></br>
+      <a target="_blank" href="https://github.com/kimjust6/Multimedia-Elements-for-UIs/blob/main/src/components/assignments/Assignment3P5.js" >Here</a> is a link to the p5.js code.
+
     </div>
   );
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+
+
 };
 
 
--- src/components/assignments/Assignment3P5.js
+++ src/components/assignments/Assignment3P5.js
@@ -2,52 +2,157 @@ import React from "react";
 import Sketch from "react-p5";
 import "./assignment3.scss";
 
-// cookie
-let x = 100;
-let y = 100;
-let x2 = 150;
-let y2 = 150;
+// start position
+var snakeX = [];
+var snakeY = [];
 
-let canvasX = 1000;
-let canvasY = 600;
+var snakeLength = 3;
+var gameOver = false;
+
+let canvasX = 1200;
+let canvasY = 500;
+let foodX = 0;
+let foodY = 0;
 var image = null;
+var image2 = null;
+const frameRate = 120;
+const blockSize = 25;
+var xDisplacement = blockSize;
+var yDisplacement = 0;
+var counter = 0;
 
 const A3P5 = (props) => {
   const setup = (p5, canvasParentRef) => {
+    snakeX[0] = 0;
+    snakeY[0] = 0;
+
     // use parent to render the canvas in this ref
     // (without that p5 will render the canvas outside of your component)
-   var cnv =  p5.createCanvas(canvasX, canvasY).parent(canvasParentRef);
+    var cnv = p5.createCanvas(canvasX, canvasY).parent(canvasParentRef);
     // image = p5.loadImage("https://i.imgur.com/lwIysss.png");
-
-    p5.textSize(20);
-    p5.text("Press any key to display it on the screen", 10, 20);
-    p5.text("Any of the action keys would not be recognized", 10, 40);
+    image2 = p5.loadImage("https://i.imgur.com/xIouWKA.png");
+    // p5.background(51);
+    p5.frameRate(frameRate);
     cnv.mousePressed((event) => {
-        console.log("Clicked on the canvas. Event:", event);
-      })
-        
+      console.log("Clicked on the canvas. Event:", event);
+    });
+
+    //get a random position for the timbit/food
+    foodX = getRandomInt(0, canvasX / blockSize);
+    foodY = getRandomInt(0, canvasY / blockSize);
     // };
   };
 
-
-  const keyPressed = (p5,key) =>
-  {
-    console.log(key.keyCode);
-  }
+  //this function gets called when there a key is pressed
+  const keyPressed = (p5, key) => {
+    if (key.keyCode === 40) {
+      //up
+      if (snakeY[1] !== snakeY[0] + blockSize) {
+        xDisplacement = 0;
+        yDisplacement = blockSize;
+      }
+    } else if (key.keyCode === 38) {
+      //down
+      if (snakeY[1] !== snakeY[0] - blockSize) {
+        xDisplacement = 0;
+        yDisplacement = -blockSize;
+      }
+    } else if (key.keyCode === 39) {
+      //right
+      if (snakeX[1] !== snakeX[0] + blockSize) {
+        xDisplacement = blockSize;
+        yDisplacement = 0;
+      }
+    } else if (key.keyCode === 37) {
+      //left
+      if (snakeX[1] !== snakeX[0] - blockSize) {
+        xDisplacement = -blockSize;
+        yDisplacement = 0;
+      }
+    }
+  };
 
   const draw = (p5) => {
-      
-
-    // p5.background(51);
+    if (!gameOver) {
+      //check if the snake ate itself
+      var i = 1;
+      var snakeEaten = false;
+      for (; i < snakeLength; i++) {
+        if (snakeX[0] === snakeX[i] && snakeY[0] === snakeY[i]) {
+          snakeEaten = true;
+        }
+      }
+      //check if game is over
+      if (
+        snakeEaten ||
+        snakeX[0] > canvasX ||
+        snakeX[0] < 0 ||
+        snakeY[0] > canvasY ||
+        snakeY[0] < 0
+      ) {
+        gameOver = true;
+      } else {
+        if (
+          foodX * blockSize === snakeX[0] &&
+          foodY * blockSize === snakeY[0]
+        ) {
+          // get a random spot for the timbit/food
+          foodX = getRandomInt(0, canvasX / blockSize - 1);
+          foodY = getRandomInt(0, canvasY / blockSize - 1);
+          snakeLength++;
+        }
+        //draw the background
+        p5.background(51);
+        p5.textSize(30);
+        p5.fill(255, 255, 255);
+        
+        p5.text("Score: " + (snakeLength - 3), 10, 25);
+        //draw the food
+        p5.image(
+          image2,
+          foodX * blockSize,
+          foodY * blockSize,
+          blockSize,
+          blockSize
+        );
+        //draw the snake
+        for (i = 0; i < snakeLength; i++) {
+          //   console.log(i + ': ' + snakeX[i]);
+          p5.fill(0, 102, 153);
+          p5.square(snakeX[i], snakeY[i], blockSize);
+        }
+        //only draw once every 5 frames
+        if (counter === 5) {
+          counter = 0;
+          //loop through the entire snake
+          i = snakeLength - 1;
+          for (; i >= 0; i--) {
+            snakeX[i + 1] = snakeX[i];
+            snakeY[i + 1] = snakeY[i];
+          }
+          snakeX[0] += xDisplacement;
+          snakeY[0] += yDisplacement;
+        } else {
+          counter++;
+        }
+      }
+    } else {
+      p5.background(51);
+      p5.textSize(128);
+      p5.fill(0, 102, 153);
+      p5.text("Game Over!", 250, 325);
+      p5.textSize(64);
+      p5.text("Score: " + (snakeLength - 3), 475, 400);
+    }
   };
-  
 
-  
-  return <Sketch setup={setup} draw={draw} keyPressed={keyPressed}/>;
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  return <Sketch setup={setup} draw={draw} keyPressed={keyPressed} />;
 };
-
-// function myInputEvent() {
-//   console.log("you are typing: ", this.value());
-// }
 
 export default A3P5;
